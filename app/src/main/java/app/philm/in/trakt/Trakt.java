73a69adcff659dc6f5ff761bf482fb6e10c68285
Chris Banes <chrisbane@google.com>
2013-12-16 09:06:00 +0000

    Add new methods to User Service

--- app/src/main/java/app/philm/in/trakt/Trakt.java
+++ app/src/main/java/app/philm/in/trakt/Trakt.java
@@ -5,4 +5,9 @@ public class Trakt extends com.jakewharton.trakt.Trakt {
     public MoviesService moviesService() {
         return buildRestAdapter().create(MoviesService.class);
     }
+
+    public PhilmUserService philmUserService() {
+        return buildRestAdapter().create(PhilmUserService.class);
+    }
+
 }

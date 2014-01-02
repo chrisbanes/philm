2db9079d06ef775f9f0d1b538cb6b60a924e4953
Chris Banes <chris@senab.co.uk>
2014-01-02 16:54:35 +0000

    Add BackgroundCallRunnable for database threading

---
+++ app/src/main/java/app/philm/in/network/BackgroundCallRunnable.java
@@ -0,0 +1,50 @@
+package app.philm.in.network;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Process;
+
+public abstract class BackgroundCallRunnable<R> implements Runnable {
+
+    private static final Handler sHandler = new Handler(Looper.getMainLooper());
+
+    @Override
+    public final void run() {
+        Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+
+        sHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                onPreBackgroundCall();
+            }
+        });
+
+        R result = doBackgroundCall();
+
+        sHandler.post(new ResultCallback(result));
+    }
+
+    public void onPreBackgroundCall() {}
+
+    public abstract R doBackgroundCall();
+
+    public void onSuccess(R result) {}
+
+    public void onFinished() {}
+
+    private class ResultCallback implements Runnable {
+        private final R mResult;
+
+        private ResultCallback(R result) {
+            mResult = result;
+        }
+
+        @Override
+        public void run() {
+            if (mResult != null) {
+                onSuccess(mResult);
+            }
+            onFinished();
+        }
+    }
+ }

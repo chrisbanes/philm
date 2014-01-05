ec66702db1c20c59bec84aa855ee94fbc507f741
Chris Banes <chris@senab.co.uk>
2014-01-05 09:01:13 +0000

    v0.1.2

--- app/src/main/java/app/philm/in/state/PhilmSQLiteOpenHelper.java
+++ app/src/main/java/app/philm/in/state/PhilmSQLiteOpenHelper.java
@@ -24,7 +24,7 @@ public class PhilmSQLiteOpenHelper extends SQLiteOpenHelper {
     private static String LOG_TAG = PhilmSQLiteOpenHelper.class.getSimpleName();
 
     private static final String DATABASE_NAME = "philm.db";
-    private static final int DATABASE_VERSION = 10;
+    private static final int DATABASE_VERSION = 11;
 
     static {
         // register our models
@@ -52,7 +52,7 @@ public class PhilmSQLiteOpenHelper extends SQLiteOpenHelper {
         // Note that existing columns will not be converted
         cupboard().withDatabase(db).upgradeTables();
 
-        if (oldVersion <= 9) {
+        if (oldVersion <= 10) {
             deleteAllPhilmMovies();
         }
     }

9d1042c94d4fcce615c25bc969e0862011a4e47f
Chris Banes <chrisbanes@google.com>
2014-01-07 14:41:03 +0000

    Add TMDB libs

--- lib/src/main/java/app/philm/in/Constants.java
+++ lib/src/main/java/app/philm/in/Constants.java
@@ -3,6 +3,7 @@ package app.philm.in;
 public class Constants {
 
     public static final String TRAKT_API_KEY = "8221f3109b8d71c2fed89dee7bad5850";
+    public static final String TMDB_API_KEY = "f413bc4bacac8dff174a909f8ef535ae";
 
     public static final String TRAKT_ACCOUNT_TYPE = "app.philm.in.account";
     public static final String TRAKT_AUTHTOKEN_PASSWORD_TYPE = "password";

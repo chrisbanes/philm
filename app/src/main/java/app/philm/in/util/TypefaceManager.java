415e75696501fa05577f8a774db84da8647e57e8
Chris Banes <chrisbanes@google.com>
2014-04-14 17:08:28 +0100

    Use native fonts if they're available

--- app/src/main/java/app/philm/in/util/TypefaceManager.java
+++ app/src/main/java/app/philm/in/util/TypefaceManager.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v4.util.LruCache;
 
 public class TypefaceManager {
@@ -14,23 +15,35 @@ public class TypefaceManager {
     private static final String ROBOTO_CONDENSED_LIGHT_FILENAME = "RobotoCondensed-Light.ttf";
     private static final String ROBOTO_SLAB_FILENAME = "RobotoSlab-Regular.ttf";
 
+    private static final String ROBOTO_LIGHT_NATIVE_FONT_FAMILY = "sans-serif-light";
+    private static final String ROBOTO_CONDENSED_NATIVE_FONT_FAMILY = "sans-serif-condensed";
+
     private final LruCache<String, Typeface> mCache;
     private final AssetManager mAssetManager;
 
     public TypefaceManager(AssetManager assetManager) {
         mAssetManager = Preconditions.checkNotNull(assetManager, "assetManager cannot be null");
-        mCache = new LruCache<>(5);
+        mCache = new LruCache<>(3);
     }
 
     public Typeface getRobotoLight() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            return Typeface.create(ROBOTO_LIGHT_NATIVE_FONT_FAMILY, Typeface.NORMAL);
+        }
         return getTypeface(ROBOTO_LIGHT_FILENAME);
     }
 
     public Typeface getRobotoCondensed() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            return Typeface.create(ROBOTO_CONDENSED_NATIVE_FONT_FAMILY, Typeface.NORMAL);
+        }
         return getTypeface(ROBOTO_CONDENSED_FILENAME);
     }
 
     public Typeface getRobotoCondensedBold() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            return Typeface.create(ROBOTO_CONDENSED_NATIVE_FONT_FAMILY, Typeface.BOLD);
+        }
         return getTypeface(ROBOTO_CONDENSED_BOLD_FILENAME);
     }
 

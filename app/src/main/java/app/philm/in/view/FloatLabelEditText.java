a8b502fe3914a72d24876a32efb3d07a410a9734
Chris Banes <chrisbanes@google.com>
2014-04-23 17:32:49 +0100

    Start of float labels

---
+++ app/src/main/java/app/philm/in/view/FloatLabelEditText.java
@@ -0,0 +1,113 @@
+package app.philm.in.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import app.philm.in.R;
+import app.philm.in.util.TextUtils;
+
+public class FloatLabelEditText extends LinearLayout implements TextWatcher {
+
+    private static final float PADDING_LEFT_RIGHT_DP = 8f;
+
+    private EditText mEditText;
+    private TextView mLabel;
+
+    public FloatLabelEditText(Context context) {
+        this(context, null);
+    }
+
+    public FloatLabelEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FloatLabelEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        setOrientation(VERTICAL);
+
+        final TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.FloatLabelEditText);
+
+        final int padding = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                PADDING_LEFT_RIGHT_DP,
+                getResources().getDisplayMetrics());
+
+        mLabel = new FontTextView(context);
+        mLabel.setPadding(padding, 0, padding, 0);
+        mLabel.setVisibility(INVISIBLE);
+
+        final int labelStyle = array.getResourceId(R.styleable.FloatLabelEditText_floatLabelTextAppearance, 0);
+        if (labelStyle != 0) {
+            mLabel.setTextAppearance(context, labelStyle);
+        }
+
+        addView(mLabel, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+        array.recycle();
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if (getChildCount() == 0) {
+            super.addView(child, index, params);
+        } else if (getChildCount() == 1) {
+            if (child instanceof EditText) {
+                super.addView(child, index, params);
+                setEditText((EditText) child);
+            } else {
+                throw new IllegalArgumentException("Child must be a EditText");
+            }
+        } else {
+            throw new IllegalArgumentException("Can not have more than 1 child");
+        }
+    }
+
+    private void setEditText(EditText editText) {
+        mEditText = editText;
+        mEditText.addTextChangedListener(this);
+
+        mLabel.setText(mEditText.getHint());
+    }
+
+    public EditText getEditText() {
+        return mEditText;
+    }
+
+    public TextView getLabel() {
+        return mLabel;
+    }
+
+    @Override
+    public final void afterTextChanged(Editable s) {
+        if (!TextUtils.isEmpty(s)) {
+            if (mLabel.getVisibility() != View.VISIBLE) {
+                mLabel.setVisibility(View.VISIBLE);
+                mLabel.setAlpha(0f);
+                mLabel.setTranslationY(mLabel.getHeight());
+                mLabel.animate().alpha(1).translationY(0).setDuration(100).start();
+            }
+        } else {
+            mLabel.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public final void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // NO-OP
+    }
+
+    @Override
+    public final void onTextChanged(CharSequence s, int start, int before, int count) {
+        // NO-OP
+    }
+}

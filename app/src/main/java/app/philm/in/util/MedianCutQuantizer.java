0a28524ac74d42275107f417a2be131659b3009f
Chris Banes <chrisbanes@google.com>
2014-06-18 09:28:42 -0700

    Update copyright

--- app/src/main/java/app/philm/in/util/MedianCutQuantizer.java
+++ app/src/main/java/app/philm/in/util/MedianCutQuantizer.java
@@ -1,16 +1,20 @@
-package app.philm.in.util;
-
-/**
- * This sample code is made available as part of the book "Digital Image
- * Processing - An Algorithmic Introduction using Java" by Wilhelm Burger
- * and Mark J. Burge, Copyright (C) 2005-2008 Springer-Verlag Berlin,
- * Heidelberg, New York.
- * Note that this code comes with absolutely no warranty of any kind.
- * See http://www.imagingbook.com for details and licensing conditions.
+/*
+ * Copyright 2014 Chris Banes
  *
- * Date: 2007/11/10
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+package app.philm.in.util;
 
 import android.graphics.Color;
 import android.util.Log;

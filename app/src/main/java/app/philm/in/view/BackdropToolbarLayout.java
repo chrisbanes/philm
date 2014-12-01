b3f25fdf240ead682279435187459f16c21cc0c2
Chris Banes <chrisbanes@google.com>
2014-12-01 15:22:12 +0000

    Tweaks for BackdropToolbarLayout

--- app/src/main/java/app/philm/in/view/BackdropToolbarLayout.java
+++ app/src/main/java/app/philm/in/view/BackdropToolbarLayout.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.Toolbar;
@@ -34,11 +35,19 @@ import android.widget.FrameLayout;
 
 import app.philm.in.R;
 
-import static app.philm.in.util.AnimationUtils.interpolate;
-
 public class BackdropToolbarLayout extends FrameLayout {
 
-    private static final float DEFAULT_MIN_TEXT_SIZE = 32f;
+    private static final float DEFAULT_MIN_TEXT_SIZE = 12f; // 12dp
+
+    private static final boolean DEBUG_DRAW = false;
+    private static final Paint DEBUG_DRAW_PAINT;
+    static {
+        DEBUG_DRAW_PAINT = DEBUG_DRAW ? new Paint() : null;
+        if (DEBUG_DRAW_PAINT != null) {
+            DEBUG_DRAW_PAINT.setAntiAlias(true);
+            DEBUG_DRAW_PAINT.setColor(Color.MAGENTA);
+        }
+    }
 
     private static final Rect TEMP_RECT = new Rect();
 
@@ -48,25 +57,26 @@ public class BackdropToolbarLayout extends FrameLayout {
     private float mScrollOffset;
 
     private final Rect mToolbarContentBounds;
-    private final Rect mCurrentTextBounds;
+    private final Rect mTextPaintBounds;
+    private final Rect mDrawnTextBounds;
 
-    private final float mMinTextSize;
+    private float mMinTextSize;
 
-    private float mInitialTitleMargin;
-    private float mRequestedInitialTitleTextSize;
+    private float mExpandedMargin;
+    private float mRequestedExpandedTitleTextSize;
+    private float mExpandedTitleTextSize;
+    private float mRequestedCollapsedTitleTextSize;
+    private float mCollapsedTitleTextSize;
 
-    private float mInitialTitleTextSize;
-    private float mRequestedFinalTitleTextSize;
-    private float mFinalTitleTextSize;
-
-    private float mInitialTop;
-    private float mFinalTop;
+    private float mExpandedTop;
+    private float mCollapsedTop;
 
     private String mTitle;
 
     private String mTitleToDraw;
 
     private float mTextLeft;
+    private float mTextRight;
     private float mTextTop;
 
     private float mScale;
@@ -92,18 +102,25 @@ public class BackdropToolbarLayout extends FrameLayout {
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BackdropToolbarLayout);
 
-        mInitialTitleMargin = a.getDimensionPixelSize(
-                R.styleable.BackdropToolbarLayout_titleInitialMargin, 0);
-        mRequestedInitialTitleTextSize = a.getDimensionPixelSize(
-                R.styleable.BackdropToolbarLayout_titleInitialTextSize, 0);
-        mRequestedFinalTitleTextSize = a.getDimensionPixelSize(
-                R.styleable.BackdropToolbarLayout_titleFinalTextSize, 0);
+        mExpandedMargin = a.getDimensionPixelSize(
+                R.styleable.BackdropToolbarLayout_expandedMargin, 0);
+        mRequestedExpandedTitleTextSize = a.getDimensionPixelSize(
+                R.styleable.BackdropToolbarLayout_expandedTextSize, 0);
+        mRequestedCollapsedTitleTextSize = a.getDimensionPixelSize(
+                R.styleable.BackdropToolbarLayout_collapsedTextSize, 0);
         mTextPaint.setColor(a.getColor(
                 R.styleable.BackdropToolbarLayout_android_textColor, Color.WHITE));
 
+        final int defaultMinTextSize = (int) TypedValue
+                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_MIN_TEXT_SIZE,
+                        getResources().getDisplayMetrics());
+        mMinTextSize = a.getDimensionPixelSize(R.styleable.BackdropToolbarLayout_minTextSize,
+                defaultMinTextSize);
+
         a.recycle();
 
-        mCurrentTextBounds = new Rect();
+        mTextPaintBounds = new Rect();
+        mDrawnTextBounds = new Rect();
         mToolbarContentBounds = new Rect();
 
         setWillNotDraw(false);
@@ -130,38 +147,35 @@ public class BackdropToolbarLayout extends FrameLayout {
     private void calculateOffsets() {
         final float offset = mScrollOffset;
 
-        mTextTop = interpolate(mInitialTop, mFinalTop, offset);
-        mTextLeft = interpolate(mInitialTitleMargin, mToolbarContentBounds.left, offset);
-        setInterpolatedTextSize(interpolate(mInitialTitleTextSize, mFinalTitleTextSize, offset));
+        mTextLeft = interpolate(mExpandedMargin, mToolbarContentBounds.left, offset);
+        mTextTop = interpolate(mExpandedTop, mCollapsedTop, offset);
+        mTextRight = interpolate(getWidth() - mExpandedMargin, mToolbarContentBounds.right, offset);
+
+        setInterpolatedTextSize(interpolate(mExpandedTitleTextSize,
+                mCollapsedTitleTextSize, offset));
 
         ViewCompat.postInvalidateOnAnimation(this);
     }
 
     private void calculateTextBounds() {
-        mInitialTitleTextSize = getSingleLineTextSize(mTitle, mTextPaint,
-                getWidth() - (mInitialTitleMargin * 2f), 0f,
-                mRequestedInitialTitleTextSize, 0.5f, getResources().getDisplayMetrics());
+        final DisplayMetrics metrics = getResources().getDisplayMetrics();
 
-        if (mInitialTitleTextSize < mMinTextSize) {
-            mInitialTitleTextSize = mMinTextSize;
-            mTextPaint.setTextSize(mInitialTitleTextSize);
+        // First, let's calculate the expanded text size so that it fit within the bounds
+        // We make sure this value is at least our minimum text size
+        mExpandedTitleTextSize = Math.max(mMinTextSize,
+                getSingleLineTextSize(mTitle, mTextPaint, getWidth() - (mExpandedMargin * 2f), 0f,
+                        mRequestedExpandedTitleTextSize, 0.5f, metrics));
+        mTextPaint.setTextSize(mExpandedTitleTextSize);
+        mTextPaint.getTextBounds(mTitle, 0, mTitle.length(), TEMP_RECT);
+        mExpandedTop = getHeight() - TEMP_RECT.height() - mExpandedMargin;
 
-            mTitleToDraw = TextUtils.ellipsize(mTitle, mTextPaint,
-                    getWidth() - (mInitialTitleMargin * 2f),
-                    TextUtils.TruncateAt.END).toString();
-        } else {
-            mTitleToDraw = mTitle;
-            mTextPaint.setTextSize(mInitialTitleTextSize);
-        }
-        mTextPaint.getTextBounds(mTitleToDraw, 0, mTitleToDraw.length(), TEMP_RECT);
-        mInitialTop = getHeight() - TEMP_RECT.height() - mInitialTitleMargin;
-
-        mFinalTitleTextSize = getSingleLineTextSize(mTitleToDraw, mTextPaint,
-                mToolbarContentBounds.width(),
-                0f, mRequestedFinalTitleTextSize, 0.5f, getResources().getDisplayMetrics());
-        mTextPaint.setTextSize(mFinalTitleTextSize);
-        mTextPaint.getTextBounds(mTitleToDraw, 0, mTitleToDraw.length(), TEMP_RECT);
-        mFinalTop = mToolbarContentBounds.centerY() - (TEMP_RECT.height() / 2f);
+        // We then calculate the collapsed text size, using the same logic
+        mCollapsedTitleTextSize = Math.max(mMinTextSize,
+                getSingleLineTextSize(mTitle, mTextPaint, mToolbarContentBounds.width(), 0f,
+                        mRequestedCollapsedTitleTextSize, 0.5f, metrics));
+        mTextPaint.setTextSize(mCollapsedTitleTextSize);
+        mTextPaint.getTextBounds(mTitle, 0, mTitle.length(), TEMP_RECT);
+        mCollapsedTop = mToolbarContentBounds.centerY() - (TEMP_RECT.height() / 2f);
     }
 
     @Override
@@ -170,32 +184,57 @@ public class BackdropToolbarLayout extends FrameLayout {
 
         if (mTitleToDraw != null) {
             canvas.save();
-            float x = -mCurrentTextBounds.left + mTextLeft;
-            float y = -mCurrentTextBounds.top + mTextTop;
+            float x = -mDrawnTextBounds.left + mTextLeft;
+            float y = -mDrawnTextBounds.top + mTextTop;
+
+            if (DEBUG_DRAW) {
+                // Just a debug tool, which drawn a Magneta rect in the text bounds
+                canvas.drawRect(mTextLeft, mTextTop, mTextRight,
+                        mTextTop + mDrawnTextBounds.height(),
+                        DEBUG_DRAW_PAINT);
+            }
+
             if (mScale != 1f) {
                 canvas.scale(mScale, mScale, x, y);
             }
+
             canvas.drawText(mTitleToDraw, x, y, mTextPaint);
             canvas.restore();
         }
     }
 
-    private void setInterpolatedTextSize(float textSize) {
-        final int pxSize = Math.round(textSize);
+    private void setInterpolatedTextSize(final float textSize) {
+        if (mTitle == null) return;
 
-        if (pxSize == mFinalTitleTextSize || pxSize == mInitialTitleTextSize || pxSize % 16 == 0) {
+        if (isCloseToDecimal(textSize) || isClose(textSize, mCollapsedTitleTextSize)
+                || isClose(textSize, mExpandedTitleTextSize) || mTitleToDraw == null) {
+            // If the text size is 'close' to being a decimal, then we use this as a sync-point.
+            // We disable our manual scaling and set the paint's text size.
             mTextPaint.setTextSize(textSize);
             mScale = 1f;
-        } else {
-            mScale = textSize / mTextPaint.getTextSize();
-        }
 
-        if (mTitleToDraw != null) {
-            mTextPaint.getTextBounds(mTitleToDraw, 0, mTitleToDraw.length(), mCurrentTextBounds);
-            mCurrentTextBounds.left *= mScale;
-            mCurrentTextBounds.top *= mScale;
-            mCurrentTextBounds.right *= mScale;
-            mCurrentTextBounds.bottom *= mScale;
+            // We also use this as an opportunity to ellipsize the string
+            final CharSequence title = TextUtils.ellipsize(mTitle, mTextPaint,
+                    (mTextRight - mTextLeft),
+                    TextUtils.TruncateAt.END);
+            if (title != mTitleToDraw) {
+                // If the title has changed, turn it into a string
+                mTitleToDraw = title.toString();
+            }
+
+            // As we've changed the text size (and possibly the text) we'll re-measure the text
+            mTextPaint.getTextBounds(mTitleToDraw, 0, mTitleToDraw.length(), mTextPaintBounds);
+            mDrawnTextBounds.set(mTextPaintBounds);
+        } else {
+            // We're not close to a decimal so use our canvas scaling method
+            mScale = textSize / mTextPaint.getTextSize();
+
+            // Because we're scaling using canvas, we need to update the drawn text bounds too
+            mDrawnTextBounds.set(mTextPaintBounds);
+            mDrawnTextBounds.left *= mScale;
+            mDrawnTextBounds.top *= mScale;
+            mDrawnTextBounds.right *= mScale;
+            mDrawnTextBounds.bottom *= mScale;
         }
 
         ViewCompat.postInvalidateOnAnimation(this);
@@ -205,24 +244,30 @@ public class BackdropToolbarLayout extends FrameLayout {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
-        if (changed) {
-            mToolbarContentBounds.left = mDummyView.getLeft();
-            mToolbarContentBounds.top = mDummyView.getTop();
-            mToolbarContentBounds.right = mDummyView.getRight();
-            mToolbarContentBounds.bottom = mDummyView.getBottom();
+        mToolbarContentBounds.left = mDummyView.getLeft();
+        mToolbarContentBounds.top = mDummyView.getTop();
+        mToolbarContentBounds.right = mDummyView.getRight();
+        mToolbarContentBounds.bottom = mDummyView.getBottom();
 
-            if (mTitle != null) {
-                calculateTextBounds();
-                calculateOffsets();
-            }
+        if (changed && mTitle != null) {
+            // If we've changed and we have a title, re-calculate everything!
+            calculateTextBounds();
+            calculateOffsets();
         }
     }
 
+    /**
+     * Set the title to display
+     *
+     * @param title
+     */
     public void setTitle(String title) {
         if (title == null || !title.equals(mTitle)) {
             mTitle = title;
 
             if (getHeight() > 0) {
+                // If we've already been laid out, calculate everything now otherwise we'll wait
+                // until a layout
                 calculateTextBounds();
                 calculateOffsets();
             }
@@ -231,6 +276,8 @@ public class BackdropToolbarLayout extends FrameLayout {
 
     /**
      * Recursive binary search to find the best size for the text
+     *
+     * Adapted from https://github.com/grantland/android-autofittextview
      */
     private static float getSingleLineTextSize(String text, TextPaint paint, float targetWidth,
             float low, float high, float precision, DisplayMetrics metrics) {
@@ -248,5 +295,29 @@ public class BackdropToolbarLayout extends FrameLayout {
         } else {
             return mid;
         }
+    }
+
+    /**
+     * Returns true if {@code value} is 'close' to it's closest decimal value. Close is currently
+     * defined as it's difference being < 0.01.
+     */
+    private static boolean isCloseToDecimal(float value) {
+        final float absValue = Math.abs(value);
+        return Math.abs(absValue - Math.round(absValue)) < 0.01f;
+    }
+
+    /**
+     * Returns true if {@code value} is 'close' to it's closest decimal value. Close is currently
+     * defined as it's difference being < 0.01.
+     */
+    private static boolean isClose(float value, float targetValue) {
+        return Math.abs(value - targetValue) < 0.01f;
+    }
+
+    /**
+     * Interpolate between {@code startValue} and {@code endValue}, using {@code progress}.
+     */
+    private static float interpolate(float startValue, float endValue, float progress) {
+        return startValue + ((endValue - startValue) * progress);
     }
 }
